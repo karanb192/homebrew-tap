e83bd3faba2c10a834013fc31984fcec81138841
karanb192 <karanb192@users.noreply.github.com>
2026-08-27 09:05:11 +0530

    Crema 0.1.3: transient batch rules

--- Casks/crema.rb
+++ Casks/crema.rb
@@ -1,6 +1,6 @@
 cask "crema" do
-  version "0.1.2"
-  sha256 "4c6a643b7752dd52739b0a227f8841664abfc184314a6384c23e96b5cfc8bb5a"
+  version "0.1.3"
+  sha256 "8b0433b897f27eb4d371564839591cd031e40c576bb055c5d5058ebe5873ad63"
 
   url "https://github.com/karanb192/crema/releases/download/v#{version}/Crema-#{version}-arm64-mac.zip"
   name "Crema"
